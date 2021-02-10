a2f126b4b88936e14c07759e058358e41d20bf12
shunsukekanno <zm19094@josai.ac.jp>
2021-02-10 09:53:17 +0000

    2/10

--- ex06-12.js
+++ ex06-12.js
@@ -1,4 +1,4 @@
 function changeImageAndBack(){imgback612();ThreeSecChange612();}
 function ThreeSecChange612(){setTimeout("imgchange612()",3000);}
 function imgchange612(){document.getElementById('img13').src = './image/josai.png';}
-function imgback612(){document.getElementById('img13').src = './image/bulbon.gif';}
+function imgback612(){document.getElementById('img13').src = './image/Thumbs_up.jpg';}
